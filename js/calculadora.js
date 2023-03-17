5d4422d3546c493747219859af9e84246da89e72
AliceBispo <98561870+AliceBispo@users.noreply.github.com>
2023-03-17 18:19:21 -0300

    Update calculadora.js

--- js/calculadora.js
+++ js/calculadora.js
@@ -46,18 +46,3 @@ function dividir() {
   document.getElementById("resultadoDiv").innerHTML = resultado;
 }
 
-function media(){
-     let nota1 = parseFloat(document.getElementById("nota1").value);
-     let nota2 = parseFloat(document.getElementById("nota2").value);
-
-     let media = (nota1 + nota2)/2 ;
-
-     if(media >= 7)
-      if(media==10)
-       alert("Uau! Aprovado com distin��o");
-      else
-       alert("Parabens, aprovado! Media "+media);
-     else
-      alert("Reprovado!")
-
-    }
