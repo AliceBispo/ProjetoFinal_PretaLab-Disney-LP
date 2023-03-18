9fbfb70da82acf6c8179541d1d40dd9152c43224
AliceBispo <98561870+AliceBispo@users.noreply.github.com>
2023-03-18 00:02:46 -0300

    Update media.js

--- js/media.js
+++ js/media.js
@@ -22,23 +22,27 @@ function calcularMedia() {
   
     const calcularMedia = (nota1 + note2 + note3 + note4)/4;
     
-    if(nota1 && note2 && note3 && note4 <0 || nota1 && note2 && note3 && note4 >10 ){
+    if(nota1 || note2 || note3 || note4 <0 || nota1 || note2 || note3 || note4 >10 ){
         alert ("A nota informada está errado ou não é compatível. Insira uma nota que seja maior que 0 e menor que 10.")
     } else {
     if (calcularMedia == 0){
         alert("Infelizmente você zerou a prova :( ");
     } else if (calcularMedia > 0 && calcularMedia < 3 ){
-        alert( "Caramba, deu ruim, você obteve media " + calcularMedia + "Estude mais e tente novamente!");
+        alert( "Caramba, deu ruim, você obteve media  " + calcularMedia + ".Estude mais e tente novamente!");
     } else if (calcularMedia > 3 && calcularMedia < 6){
-        alert( "Você obteve media " + calcularMedia + "Falta pouco para a média.");
+        alert( "Você obteve media  " + calcularMedia + ".Falta pouco para a média.");
     } else if (calcularMedia >= 6 && calcularMedia <= 7){
-        alert("Você está na média com " + calcularMedia);
+        alert("Você está na média com  " + calcularMedia + ".");
     } else if (calcularMedia > 7 && calcularMedia <10){
-        alert("Notão! Sua média é " + calcularMedia );
+        alert("Notão! Sua média é  " + calcularMedia + "!");
     } else{
         alert ("Hoje é seu aniversário? Pq você ta de parabéns! 10 de média")
     }
     }
+  
+    // evite mexer na linha de código abaixo!
+    document.getElementById("situacaoAluno").innerHTML = mensagem;
+  }
   
     // evite mexer na linha de código abaixo!
     document.getElementById("situacaoAluno").innerHTML = mensagem;
